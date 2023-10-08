fe816ffe5e066514cd3c6eed51df795298cf7212
Prashant <prashantco111@gmail.com>
2023-10-08 11:25:28 +0530

    CHORE: Fixed mobile responsive

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -15,7 +15,7 @@ function BlogPage({ params }: { params: { id: string } }) {
 		const blog: IBlog = BlogsService.getBlog(parseInt(params.id))
 		const user: IUser = UsersService.getUser(blog.userId)
 		return (
-			<div className="max-w-5xl px-6 py-10 mx-auto space-y-12 dark:bg-gray-800 dark:text-gray-50">
+			<div className="max-w-5xl px-6 mx-auto space-y-12 dark:bg-gray-800 dark:text-gray-50">
 				<div>
 					<Link
 						className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-50"
